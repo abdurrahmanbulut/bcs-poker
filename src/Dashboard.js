b4c5148917b0f913f08407c2b8cb5cc955fb49fc
Abdurrahman BULUT <abdurrahmanbulut08@gmail.com>
2023-10-27 23:28:14 +0300

    dashboard-login added

---
+++ src/Dashboard.js
@@ -0,0 +1,305 @@
+import React, { useEffect, useState } from "react";
+import { getAuth, signOut, updateProfile } from "firebase/auth";
+import "./Dashboard.css";
+import { useNavigate } from "react-router-dom";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  update as dbUpdate,
+} from "firebase/database";
+import { set } from "firebase/database";
+
+function Dashboard() {
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  const db = getDatabase();
+  const [seats, setSeats] = useState({});
+  const [user, setUser] = useState(auth.currentUser);
+  const [selectedNumber, setSelectedNumber] = useState(null);
+  const [showCards, setShowCards] = useState(false);
+  const [average, setAverage] = useState(null);
+
+  const [isEditingUsername, setIsEditingUsername] = useState(false);
+  const [editedUsername, setEditedUsername] = useState(
+    user?.displayName || user?.email
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const seatRef = ref(db, "seats");
+      onValue(seatRef, (snapshot) => {
+        const data = snapshot.val();
+        setSeats(data || {});
+      });
+    };
+
+    fetchData();
+
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        setEditedUsername(currentUser.displayName || currentUser.email);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [db, auth]);
+
+  const updateSelectedCard = async (cardNumber) => {
+    if (user) {
+      const userSeatKey = Object.keys(seats).find(
+        (key) =>
+          seats[key].username === user.displayName ||
+          seats[key].username === user.email
+      );
+
+      if (userSeatKey) {
+        const seatRef = ref(db, `seats/${userSeatKey}`);
+
+        const updatedData = {
+          username: user.displayName || user.email,
+          selectedCard: cardNumber,
+        };
+        await set(seatRef, updatedData);
+      }
+    }
+  };
+
+  const resetSelectedNumbers = async () => {
+    try {
+      setShowCards(false);
+      const updates = {};
+      Object.keys(seats).forEach((key) => {
+        updates[`seats/${key}/selectedCard`] = null;
+      });
+      await dbUpdate(ref(db), updates);
+      console.log("Seçimler başarıyla sıfırlandı.");
+    } catch (error) {
+      console.error("Seçimleri sıfırlarken hata oluştu:", error.message);
+    }
+  };
+
+  const handleUsernameClick = () => {
+    setIsEditingUsername(true);
+  };
+
+  const handleUsernameChange = (e) => {
+    setEditedUsername(e.target.value);
+  };
+
+  const calculateAverage = () => {
+    const selectedNumbers = Object.values(seats).map(
+      (seat) => seat.selectedCard
+    );
+    const validNumbers = selectedNumbers.filter((num) => num != null);
+    const sum = validNumbers.reduce((acc, curr) => acc + curr, 0);
+    const avg = validNumbers.length > 0 ? sum / validNumbers.length : 0;
+    setAverage(avg);
+  };
+
+  const openCards = () => {
+    setShowCards(true);
+    calculateAverage();
+  };
+
+  const handleUsernameSubmit = async () => {
+    console.log(user.displayName);
+    console.log(editedUsername);
+    console.log(true);
+    console.log(editedUsername);
+    try {
+      if (
+        user &&
+        editedUsername !== user.displayName &&
+        editedUsername !== ""
+      ) {
+        const isUsernameTaken = Object.values(seats).includes(editedUsername);
+
+        if (isUsernameTaken) {
+          alert(
+            "This username is already taken. Please choose a different one."
+          );
+          return;
+        }
+
+        const userSeatKey = Object.keys(seats).find(
+          (key) => seats[key] === user.displayName || seats[key] === user.email
+        );
+        console.log(userSeatKey);
+
+        if (userSeatKey) {
+          await set(ref(db, "seats/" + userSeatKey), editedUsername);
+        }
+        await updateProfile(user, { displayName: editedUsername });
+
+        console.log("asdd");
+        //setUser({ ...user, displayName: editedUsername });
+        setIsEditingUsername(false);
+      }
+    } catch (error) {
+      console.error("Error updating display name:", error.message);
+      alert("Error updating display name: " + error.message);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/login");
+    } catch (error) {
+      console.error("Çıkış yapılırken hata oluştu:", error.message);
+    }
+  };
+
+  const handleResetAndExit = async () => {
+    try {
+      await set(ref(db, "seats"), {});
+      await handleSignOut();
+    } catch (error) {
+      console.error("Reset and Exit failed:", error.message);
+      alert(
+        "An error occurred while trying to reset and exit. Please try again."
+      );
+    }
+  };
+
+  return (
+    <div className="dashboard">
+      <div className="title-bar">
+        {user && (
+          <>
+            {isEditingUsername ? (
+              <div className="username-edit">
+                <input
+                  className="username-input"
+                  value={editedUsername}
+                  onChange={handleUsernameChange}
+                  autoFocus
+                />
+                <button
+                  className="save-username-button"
+                  onClick={handleUsernameSubmit}
+                >
+                  Save
+                </button>
+              </div>
+            ) : (
+              <span className="center-text" onClick={handleUsernameClick}>
+                {user.displayName || user.email}
+              </span>
+            )}
+            <button onClick={handleSignOut}>Çıkış Yap</button>
+            <button onClick={handleResetAndExit}>Reset</button>
+          </>
+        )}
+      </div>
+      <div className="seating-area">
+        <div className="left-side">
+          {Object.keys(seats)
+            .slice(0, 4)
+            .map((seat, index) => (
+              <div className="person" key={index}>
+                <div className="seat">{seats[seat].username || index + 1}</div>
+                <div className="spacer"></div>
+                <div className="cards">
+                  {showCards ? (
+                    seats[seat].selectedCard
+                  ) : !seats[seat].selectedCard ? (
+                    <img
+                      src="/secret.png"
+                      width="100%"
+                      height="100%"
+                      alt="Card back"
+                    />
+                  ) : (
+                    <img
+                      src="/pokemon.png"
+                      width="170%"
+                      height="100%"
+                      alt="Card back"
+                    />
+                  )}
+                </div>
+              </div>
+            ))}
+          {[...Array(4 - Object.keys(seats).slice(0, 4).length)].map(
+            (_, index) => (
+              <div className="person empty" key={index}>
+                <div className="seat"></div>
+                <div className="spacer"></div>
+                <div className="cards"></div>
+              </div>
+            )
+          )}
+        </div>
+        <div className="right-side">
+          {Object.keys(seats)
+            .slice(4, 8)
+            .map((seat, index) => (
+              <div className="person" key={index}>
+                <div className="cards">
+                  {showCards ? (
+                    seats[seat].selectedCard
+                  ) : !seats[seat].selectedCard ? (
+                    <img
+                      src="/secret.png"
+                      width="100%"
+                      height="100%"
+                      alt="Card back"
+                    />
+                  ) : (
+                    <img
+                      src="/pokemon.png"
+                      width="170%"
+                      height="100%"
+                      alt="Card back"
+                    />
+                  )}
+                </div>
+                <div className="spacer"></div>
+                <div className="seat">{seats[seat].username || index + 5}</div>
+              </div>
+            ))}
+          {[...Array(4 - Object.keys(seats).slice(4, 8).length)].map(
+            (_, index) => (
+              <div className="person empty" key={index}>
+                <div className="cards"></div>
+                <div className="spacer"></div>
+                <div className="seat"></div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+      <div className="center-button align-column">
+        <button className="button-shape" onClick={openCards}>
+          Kartları Aç
+        </button>
+        <button className="button-shape" onClick={resetSelectedNumbers}>
+          Seçimleri Sıfırla
+        </button>
+        {average !== null && (
+          <div className="average-display">Average: {average.toFixed(2)}</div>
+        )}
+      </div>
+
+      <div className="floating-box">
+        {[1, 2, 3, 5, 8].map((number) => (
+          <div
+            key={number}
+            className="number"
+            onClick={() => updateSelectedCard(number)}
+          >
+            {number}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/Login.js
@@ -0,0 +1,155 @@
+import React, { useState } from "react";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
+import { getDatabase, ref, set, get } from "firebase/database";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
+  const db = getDatabase();
+  const navigate = useNavigate();
+
+  const signInWithEmail = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+
+      const seatsRef = ref(db, "seats");
+      const seatsSnapshot = await get(seatsRef);
+      const occupiedSeats = seatsSnapshot.val() || {};
+
+      const availableSeats = [];
+      for (let i = 1; i <= 16; i++) {
+        const seatKey = `seat${i}`;
+        if (!occupiedSeats[seatKey]) {
+          availableSeats.push(seatKey);
+        }
+      }
+
+      if (availableSeats.length === 0) {
+        alert("Masa dolu!");
+        return;
+      }
+
+      const randomIndex = Math.floor(Math.random() * availableSeats.length);
+      const userSeat = availableSeats[randomIndex];
+      await set(ref(db, "seats/" + userSeat), {
+        username: user.displayName || user.email,
+        selectedCard: null,
+      });
+
+      navigate("/dashboard");
+    } catch (error) {
+      alert("Giriş başarısız: " + error.message);
+    }
+  };
+
+  const signUpWithEmail = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+
+      const seatsRef = ref(db, "seats");
+      const seatsSnapshot = await get(seatsRef);
+      const occupiedSeats = seatsSnapshot.val() || {};
+
+      const availableSeats = [];
+      for (let i = 1; i <= 16; i++) {
+        const seatKey = `seat${i}`;
+        if (!occupiedSeats[seatKey] || !occupiedSeats[seatKey].username) {
+          availableSeats.push(seatKey);
+        }
+      }
+
+      if (availableSeats.length === 0) {
+        alert("Masa dolu!");
+        return;
+      }
+
+      const randomIndex = Math.floor(Math.random() * availableSeats.length);
+      const userSeat = availableSeats[randomIndex];
+      await set(ref(db, "seats/" + userSeat), {
+        username: user.displayName || user.email,
+        selectedCard: null,
+      });
+
+      navigate("/dashboard");
+    } catch (error) {
+      alert("Kayıt başarısız: " + error.message);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+      const user = auth.currentUser;
+
+      const seatsRef = ref(db, "seats");
+      const seatsSnapshot = await get(seatsRef);
+      const occupiedSeats = seatsSnapshot.val() || {};
+
+      const availableSeats = [];
+      for (let i = 1; i <= 16; i++) {
+        const seatKey = `seat${i}`;
+        if (!occupiedSeats[seatKey] || !occupiedSeats[seatKey].username) {
+          availableSeats.push(seatKey);
+        }
+      }
+
+      if (availableSeats.length === 0) {
+        alert("Masa dolu!");
+        return;
+      }
+
+      const randomIndex = Math.floor(Math.random() * availableSeats.length);
+      const userSeat = availableSeats[randomIndex];
+      await set(ref(db, "seats/" + userSeat), {
+        username: user.displayName || user.email,
+        selectedCard: null,
+      });
+      navigate("/dashboard");
+    } catch (error) {
+      alert("Google ile giriş başarısız: " + error.message);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h4 className="login-header">BCS-Poker</h4>
+        <div className="input-group">
+          <label className="login-header">Email</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+        </div>
+        <div className="input-group">
+          <label className="login-header">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </div>
+        <button onClick={signInWithEmail}>Giriş Yap</button>
+        <button onClick={signUpWithEmail}>Kayıt Ol</button>
+        <button onClick={signInWithGoogle}>Google ile Giriş Yap</button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
