a96bfb4f46ba5e69223b0ca69ded5c563e8af3d1
Abdurrahman BULUT <abdurrahmanbulut08@gmail.com>
2023-11-04 00:44:14 +0300

    frbs

--- src/firebase.js
+++ src/firebase.js
@@ -10,15 +10,26 @@ import {
   remove,
 } from "firebase/database";
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBVS6A7amOkgQXGHoE-uVU6pPl41eFM6to",
+  authDomain: "bcs-poker.firebaseapp.com",
+  projectId: "bcs-poker",
+  storageBucket: "bcs-poker.appspot.com",
+  messagingSenderId: "939332526335",
+  appId: "1:939332526335:web:a9c0c65d49c96191416cca",
+  measurementId: "G-NQVS5JBESR",
 };
+
+
 
 const app = initializeApp(firebaseConfig);
 
